416c3669b67b86bec435ad6b184fd8c0de7925b9
hernanai <azeemihernandez@gmail.com>
2023-08-17 17:58:35 -0500

    Updates

---
+++ precache-manifest.1beb6862b5fe4635015bf78833a7bcbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "38d378137301f283c44e",
+    "url": "/static/css/main.9802cb41.chunk.css"
+  },
+  {
+    "revision": "38d378137301f283c44e",
+    "url": "/static/js/main.38d37813.chunk.js"
+  },
+  {
+    "revision": "e7a8bed8f078f8a72f51",
+    "url": "/static/js/1.e7a8bed8.chunk.js"
+  },
+  {
+    "revision": "f9e2105f5e87aa850ca7",
+    "url": "/static/js/2.f9e2105f.chunk.js"
+  },
+  {
+    "revision": "c8a2f62ebcb612c56333",
+    "url": "/static/js/runtime~main.c8a2f62e.js"
+  },
+  {
+    "revision": "f6f71ca293dd41d0136ff7ba1ffa1a04",
+    "url": "/static/media/background.f6f71ca2.jpg"
+  },
+  {
+    "revision": "5c8fd00e6dc195f219f9583a9184c54c",
+    "url": "/static/media/JamisonMeader_Resume_.5c8fd00e.pdf"
+  },
+  {
+    "revision": "452d6ce6663ad19d19a5d2de00363b19",
+    "url": "/index.html"
+  }
+];
